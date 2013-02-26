7d2ab51208f7043827d24c6d2159368ff34f579b
Matt Heath <matt@mattheath.com>
2013-02-26 20:16:20 +0000

    Kill off the old system wide fpm service to make room for the future

--- manifests/init.pp
+++ manifests/init.pp
@@ -66,16 +66,25 @@ class php {
     require => Exec['phpenv-setup-root-repo']
   }
 
-  # Remove the PHP src repository we cached before switching to php-build
-  file { "${root}/php-src":
-    ensure => absent
-  }
-
   # This needs something to stop it running each time, rbenv class greps both
   # libexec and shims/gem
   exec { 'phpenv-rehash-post-install':
     command => "/bin/rm -rf ${root}/shims && PHPENV_ROOT=${root} ${root}/bin/phpenv rehash",
     require => Exec["ensure-phpenv-version-${phpenv_version}"],
+  }
+
+  # Remove the PHP src repository we cached before switching to php-build
+  file { "${root}/php-src":
+    ensure => absent
+  }
+
+  # Kill off the legacy PHP-FPM daemon as we're moving to per version instances
+  file { "/Library/LaunchDaemons/dev.php-fpm.plist":
+    ensure  => 'absent',
+    require => Service["dev.php-fpm"]
+  }
+  service { "dev.php-fpm":
+    ensure => stopped,
   }
 
 }
