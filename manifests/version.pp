65c79401cd556d8a801836f9382091cba4cced60
Matt Heath <matt@mattheath.com>
2013-02-27 01:13:09 +0000

    FINALLY. Install custom PHP version with boxeny config paths

--- manifests/version.pp
+++ manifests/version.pp
@@ -53,9 +53,10 @@ define php::version(
     exec { "php-install-${version}":
       command     => "${php::php_build::root}/bin/php-build ${version} ${php::config::root}/versions/${version}",
       cwd         => "${php::config::root}/versions",
+      environment => "PHP_BUILD_CONFIGURE_OPTS=--with-config-file-path=${php_ini} --with-config-file-scan-dir=${conf_d}",
       provider    => 'shell',
       timeout     => 0,
-      creates     => $dest,
+      creates     => "${dest}/bin/php",
       require     => File[$php_ini],
     }
 
