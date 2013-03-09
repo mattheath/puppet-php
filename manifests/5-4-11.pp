8dbb3577c597604efcf842200dd79e7a4612a9b2
Matt Heath <matt@mattheath.com>
2013-03-09 12:02:49 +0000

    Add 5.4.11 manifest and link 5.4 to it

---
+++ manifests/5-4-11.pp
@@ -0,0 +1,8 @@
+# Installs php 5.4.11 from phpenv/php-build
+#
+# Usage:
+#
+#     include php::5-4-11
+class php::5-4-11 {
+  php::version { '5.4.11': }
+}
--- manifests/5-4.pp
+++ manifests/5-4.pp
@@ -1,15 +1,15 @@
-# Installs php 5.4.10 from phpenv and symlinks it as 5.4
+# Installs php 5.4.11 and symlinks it as 5.4
 #
 # Usage:
 #
 #     include php::5-4
 class php::5-4 {
   require php
-  require php::5-4-10
+  require php::5-4-11
 
   file { "${php::config::root}/versions/5.4":
     ensure  => symlink,
     force   => true,
-    target  => "${ruby::root}/versions/5.4.10"
+    target  => "${ruby::root}/versions/5.4.11"
   }
 }
