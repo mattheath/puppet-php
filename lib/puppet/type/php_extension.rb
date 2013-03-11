74e2ee3ea0419918eb5773383915385caa63c24a
Matt Heath <matt@mattheath.com>
2013-03-11 11:51:00 +0000

    Add extra params to extension type

--- lib/puppet/type/php_extension.rb
+++ lib/puppet/type/php_extension.rb
@@ -17,8 +17,23 @@ Puppet::Type.newtype(:php_extension) do
     isnamevar
   end
 
+  newparam(:extension) do
+  end
+
   newparam(:version) do
     defaultto '>= 0'
+  end
+
+  newparam(:package_name) do
+  end
+
+  newparam(:package_url) do
+  end
+
+  newparam(:phpenv_root) do
+  end
+
+  newparam(:php_version) do
   end
 
 end
