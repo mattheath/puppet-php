e6fe4e2787298b50e4d30f256a9d7f25553c7ecb
Matt Heath <matt@mattheath.com>
2013-03-12 20:13:29 +0000

    Correct 5.3.18 fpm manifest to use correct php version

--- manifests/fpm/5-3-18.pp
+++ manifests/fpm/5-3-18.pp
@@ -1,10 +1,10 @@
-# Launches a PHP FPM service running PHP 5.3.20
-# Installs PHP 5.3.20 if it doesn't already exist
+# Launches a PHP FPM service running PHP 5.3.18
+# Installs PHP 5.3.18 if it doesn't already exist
 #
 # Usage:
 #
-#     include php::fpm::5-3-20
+#     include php::fpm::5-3-18
 #
-class php::fpm::5-3-20 {
-  php::fpm { '5.3.20': }
+class php::fpm::5-3-18 {
+  php::fpm { '5.3.18': }
 }
