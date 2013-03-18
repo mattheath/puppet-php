67675ef693ff7168620550f26de17c3b6eee291b
Matt Heath <matt@mattheath.com>
2013-03-18 21:11:27 +0000

    Bring pecl var naming in line with git provider

--- lib/puppet/provider/php_extension/pecl.rb
+++ lib/puppet/provider/php_extension/pecl.rb
@@ -10,7 +10,7 @@ Puppet::Type.type(:php_extension).provide(:pecl) do
   def create
 
     # Let's get a few things straight
-    @working_dir = "#{@resource[:cache_dir]}/#{@resource[:package_name]}/#{@resource[:package_name]}"
+    @work_dir = "#{@resource[:cache_dir]}/#{@resource[:package_name]}/#{@resource[:package_name]}"
     @php_version_prefix = "#{@resource[:phpenv_root]}/versions/#{@resource[:php_version]}"
     @resource[:compiled_name] ||= "#{@resource[:extension]}.so"
 
@@ -57,24 +57,24 @@ protected
 
   # PHPize the extension, using the correct version of PHP
   def phpize
-    %x( export PHP_AUTOCONF=#{autoconf} && export PHP_AUTOHEADER=#{autoheader} && cd #{@working_dir} && #{@php_version_prefix}/bin/phpize )
+    %x( export PHP_AUTOCONF=#{autoconf} && export PHP_AUTOHEADER=#{autoheader} && cd #{@work_dir} && #{@php_version_prefix}/bin/phpize )
   end
 
   # Configure with the correct version of php-config and prefix and any additional configure parameters
   def configure
-    puts "cd #{@working_dir} && ./configure --prefix=#{@php_version_prefix} --with-php-config=#{@php_version_prefix}/bin/php-config"
-    puts %x( cd #{@working_dir} && ./configure --prefix=#{@php_version_prefix} --with-php-config=#{@php_version_prefix}/bin/php-config #{@resource[:configure_params]})
+    puts "cd #{@work_dir} && ./configure --prefix=#{@php_version_prefix} --with-php-config=#{@php_version_prefix}/bin/php-config"
+    puts %x( cd #{@work_dir} && ./configure --prefix=#{@php_version_prefix} --with-php-config=#{@php_version_prefix}/bin/php-config #{@resource[:configure_params]})
   end
 
   # Make the module
   def make
-    puts %x( cd #{@working_dir} && make )
-    raise "Failed to build module #{@resource[:name]}" unless File.exists?("#{@working_dir}/modules/#{@resource[:compiled_name]}")
+    puts %x( cd #{@work_dir} && make )
+    raise "Failed to build module #{@resource[:name]}" unless File.exists?("#{@work_dir}/modules/#{@resource[:compiled_name]}")
   end
 
   # Make the module
   def install
-    %x( cp #{@working_dir}/modules/#{@resource[:compiled_name]} #{@php_version_prefix}/modules/#{@resource[:compiled_name]} )
+    %x( cp #{@work_dir}/modules/#{@resource[:compiled_name]} #{@php_version_prefix}/modules/#{@resource[:compiled_name]} )
     raise "Failed to install module #{@resource[:name]}" unless File.exists?("#{@php_version_prefix}/modules/#{@resource[:compiled_name]}")
   end
 
