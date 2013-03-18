8dda5d95367f767c9ee1b26df9538ce4012376c2
Matt Heath <matt@mattheath.com>
2013-03-18 22:00:09 +0000

    s/PKG_CONFIG_PATH/PKG_CONFIG

--- lib/puppet/provider/php_extension/git.rb
+++ lib/puppet/provider/php_extension/git.rb
@@ -73,18 +73,17 @@ protected
 
   # PHPize the extension, using the correct version of PHP
   def phpize
-    %x( export PHP_AUTOCONF=#{autoconf} && export PHP_AUTOHEADER=#{autoheader} && export PKG_CONFIG_PATH=#{@resource[:homebrew_path]}/bin/pkg-config && cd #{@work_dir} && #{@php_version_prefix}/bin/phpize )
+    %x( export PHP_AUTOCONF=#{autoconf} && export PHP_AUTOHEADER=#{autoheader} && export PKG_CONFIG=#{@resource[:homebrew_path]}/bin/pkg-config && cd #{@work_dir} && #{@php_version_prefix}/bin/phpize )
   end
 
   # Configure with the correct version of php-config and prefix and any additional configure parameters
   def configure
-    %x( export PKG_CONFIG_PATH=#{@resource[:homebrew_path]}/bin/pkg-config && cd #{@work_dir} && ./configure --prefix=#{@php_version_prefix} --with-php-config=#{@php_version_prefix}/bin/php-config #{@resource[:configure_params]})
+    %x( export PKG_CONFIG=#{@resource[:homebrew_path]}/bin/pkg-config && cd #{@work_dir} && ./configure --prefix=#{@php_version_prefix} --with-php-config=#{@php_version_prefix}/bin/php-config #{@resource[:configure_params]})
   end
 
   # Make the module
   def make
-    puts %x( cd #{@work_dir} && make )
-
+    %x( cd #{@work_dir} && make )
     raise "Failed to build module #{@resource[:name]}" unless File.exists?("#{@work_dir}/modules/#{@resource[:compiled_name]}")
   end
 
