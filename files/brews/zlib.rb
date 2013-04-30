c2c5897cb32d4291ece76463b2a3758092aed998
Hugh Evans <hugh@artpop.com.au>
2013-04-30 13:25:18 +1000

    libtool patch isn’t required for zlib 1.2.8.

--- files/brews/zlib.rb
+++ files/brews/zlib.rb
@@ -11,27 +11,9 @@ class Zlibphp < Formula
 
   option :universal
 
-  def patches; DATA; end
-
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 end
-
-# /usr/bin/libtool is no use to CLT-less rigs
-__END__
-diff --git a/configure b/configure
-index 36c7d8e..0c97d7b 100755
---- a/configure
-+++ b/configure
-@@ -231,7 +231,7 @@ if test "$gcc" -eq 1 && ($cc -c $cflags $test.c) >> configure.log 2>&1; then
-              SHAREDLIBV=libz.$VER$shared_ext
-              SHAREDLIBM=libz.$VER1$shared_ext
-              LDSHARED=${LDSHARED-"$cc -dynamiclib -install_name $libdir/$SHAREDLIBM -compatibility_version $VER1 -current_version $VER3"}
--             AR="/usr/bin/libtool"
-+             AR="libtool"
-              ARFLAGS="-o" ;;
-   *)             LDSHARED=${LDSHARED-"$cc -shared"} ;;
-   esac
