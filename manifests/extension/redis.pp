79f8932d7fcc17287b81d46aa34c7e4d1eb9f944
b00gizm <b00gizm@gmail.com>
2013-06-22 11:43:18 +0200

    Added Redis PHP extension

---
+++ manifests/extension/redis.pp
@@ -0,0 +1,53 @@
+# Installs the redis php extension for a specific version of php.
+#
+# Usage:
+#
+#     php::extension::xdebug { 'redis for 5.4.10':
+#       version   => '2.2.3'
+#       php       => '5.4.10',
+#     }
+#
+define php::extension::redis(
+  $php,
+  $version = '2.2.3'
+) {
+  require redis
+
+  require php::config
+  # Require php version eg. php::5_4_10
+  # This will compile, install and set up config dirs if not present
+  require join(['php', join(split($php, '[.]'), '_')], '::')
+
+  $extension = 'redis'
+
+  # Final module install path
+  $module_path = "${php::config::root}/versions/${php}/modules/${extension}.so"
+
+  # Clone the source repository
+  repository { "${php::config::extensioncachedir}/redis":
+    source => 'nicolasff/phpredis'
+  }
+
+  # Build & install the extension
+  php_extension { $name:
+    provider       => 'git',
+
+    extension      => $extension,
+    version        => $version,
+
+    homebrew_path  => $boxen::config::homebrewdir,
+    phpenv_root    => $php::config::root,
+    php_version    => $php,
+
+    cache_dir      => $php::config::extensioncachedir,
+    require        => Repository["${php::config::extensioncachedir}/zmq"],
+  }
+
+  # Add config file once extension is installed
+
+  file { "${php::config::configdir}/${php}/conf.d/${extension}.ini":
+    content => template('php/extensions/generic.ini.erb'),
+    require => Php_extension[$name],
+  }
+
+}
