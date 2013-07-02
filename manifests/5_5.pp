624eff9364345f29e3934975a74c3566c41e0e1d
Matt Heath <matt@mattheath.com>
2013-07-03 00:05:49 +0100

    Add PHP 5.5.0 along with FPM class

---
+++ manifests/5_5.pp
@@ -0,0 +1,15 @@
+# Installs php 5.5.0 and symlinks it as 5.5
+#
+# Usage:
+#
+#     include php::5-5
+class php::5_5 {
+  require php
+  require php::5_5_0
+
+  file { "${php::config::root}/versions/5.5":
+    ensure  => symlink,
+    force   => true,
+    target  => "${php::root}/versions/5.5.0"
+  }
+}
