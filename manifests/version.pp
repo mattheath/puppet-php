fb0c5f62f42aa965a0a9ed387c006f5912e4a6ac
Matt Heath <matt@mattheath.com>
2013-07-02 23:23:58 +0100

    Fix variable encapsulation so lint checks pass

--- manifests/version.pp
+++ manifests/version.pp
@@ -115,7 +115,7 @@ define php::version(
         Package['autoconf'],
         Package['boxen/brews/autoconf213'],
       ],
-      notify        => Exec["phpenv-rehash-post-install-$version"],
+      notify        => Exec["phpenv-rehash-post-install-${version}"],
     }
 
     # Fix permissions for php versions installed prior to 0.3.5 of this module
@@ -128,7 +128,7 @@ define php::version(
     }
 
     # Rehash phpenv shims when a new version is installed
-    exec { "phpenv-rehash-post-install-$version":
+    exec { "phpenv-rehash-post-install-${version}":
       command     => "/bin/rm -rf ${php::config::root}/shims && PHPENV_ROOT=${php::config::root} ${php::config::root}/bin/phpenv rehash",
       require     => Php_version[$version],
       refreshonly => true,
