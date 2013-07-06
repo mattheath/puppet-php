fc7bdbe7b90aae7f7c226794e35f8fb999d28cf2
Matt Heath <matt@mattheath.com>
2013-07-06 14:44:30 +0100

    Update 5.4 major version class to 5.4.17

--- manifests/5_4.pp
+++ manifests/5_4.pp
@@ -1,15 +1,15 @@
-# Installs php 5.4.16 and symlinks it as 5.4
+# Installs php 5.4.17 and symlinks it as 5.4
 #
 # Usage:
 #
 #     include php::5-4
 class php::5_4 {
   require php
-  require php::5_4_16
+  require php::5_4_17
 
   file { "${php::config::root}/versions/5.4":
     ensure  => symlink,
     force   => true,
-    target  => "${php::root}/versions/5.4.16"
+    target  => "${php::root}/versions/5.4.17"
   }
 }
