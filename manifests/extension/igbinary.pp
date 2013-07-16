abb936f4bb421aa25703bab0f54cc183812d991c
em <eugen.mayer@kontextwork.de>
2013-07-17 00:04:06 +0200

    Adding igbinry extension support

---
+++ manifests/extension/igbinary.pp
@@ -0,0 +1,45 @@
+# Installs a php extension for a specific version of php.
+#
+# Usage:
+#
+#     php::extension::apc { 'apc for 5.4.10':
+#       version   => '3.1.13'
+#       php       => '5.4.10',
+#     }
+#
+define php::extension::igbinary(
+  $version = '1.1.1',
+  $php
+) {
+  require php::config
+  # Require php version eg. php::5-4-10
+  # This will compile, install and set up config dirs if not present
+  require join(['php', join(split($php, '[.]'), '-')], '::')
+
+  $extension = 'igbinary'
+  $package_name = "igbinary-${version}"
+  $url = "http://pecl.php.net/get/igbinary-${version}.tgz"
+
+  # Final module install path
+  $module_path = "${php::config::root}/versions/${php}/modules/${extension}.so"
+
+  php_extension { $name:
+    extension      => $extension,
+    version        => $version,
+    package_name   => $package_name,
+    package_url    => $url,
+    homebrew_path  => $boxen::config::homebrewdir,
+    phpenv_root    => $php::config::root,
+    php_version    => $php,
+    cache_dir      => $php::config::extensioncachedir,
+  }
+
+  # Add config file once extension is installed
+
+  file { "${php::config::configdir}/${php}/conf.d/${extension}.ini":
+    content => template("php/extensions/${extension}.ini.erb"),
+    require => Php_extension[$name],
+  }
+
+}
+name
