262d73998d60252092005ed00e7689a23e2fa053
em <eugen.mayer@kontextwork.de>
2013-07-17 13:10:53 +0200

    use _ for the version split, instead of -

--- manifests/extension/igbinary.pp
+++ manifests/extension/igbinary.pp
@@ -14,7 +14,7 @@ define php::extension::igbinary(
   require php::config
   # Require php version eg. php::5-4-10
   # This will compile, install and set up config dirs if not present
-  require join(['php', join(split($php, '[.]'), '-')], '::')
+  require join(['php', join(split($php, '[.]'), '_')], '::')
 
   $extension = 'igbinary'
   $package_name = "igbinary-${version}"
