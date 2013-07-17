069f17be76214ceed27515bc6dcc7aa774785e9a
em <eugen.mayer@kontextwork.de>
2013-07-17 12:46:27 +0200

    use the generic template

--- manifests/extension/imagick.pp
+++ manifests/extension/imagick.pp
@@ -37,7 +37,7 @@ define php::extension::imagick(
   # Add config file once extension is installed
 
   file { "${php::config::configdir}/${php}/conf.d/${extension}.ini":
-    content => template("php/extensions/${extension}.ini.erb"),
+    content => template("php/extensions/generic.ini.erb"),
     require => Php_extension[$name],
   }
 
