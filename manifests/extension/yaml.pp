a105e6de839e653f26771f4773efdeaec7c5be0e
em <eugen.mayer@kontextwork.de>
2013-07-22 14:59:13 +0200

    extracted libyaml to dependencies, fixed code-style issues based on puppet-lint

--- manifests/extension/yaml.pp
+++ manifests/extension/yaml.pp
@@ -2,24 +2,17 @@
 #
 # Usage:
 #
-#     php::extension::apc { 'apc for 5.4.10':
-#       version   => '3.1.13'
+#     php::extension::yaml { 'yaml for 5.4.10':
 #       php       => '5.4.10',
 #     }
 #
-class libyaml {
-  include homebrew
-
-  package { 'libyaml': }
-}
 
 define php::extension::yaml(
-  $version = '1.1.0',
   $php
+  $version = '1.1.0',
 ) {
-  require php::config  
-  include libyaml
-  
+  require php::config
+  include php::dependencies::libyaml
   # Require php version eg. php::5-4-10
   # This will compile, install and set up config dirs if not present
   require join(['php', join(split($php, '[.]'), '_')], '::')
@@ -32,22 +25,22 @@ define php::extension::yaml(
   $module_path = "${php::config::root}/versions/${php}/modules/${extension}.so"
 
   php_extension { $name:
-    extension      => $extension,
-    version        => $version,
-    package_name   => $package_name,
-    package_url    => $url,
-    homebrew_path  => $boxen::config::homebrewdir,
-    phpenv_root    => $php::config::root,
-    php_version    => $php,
-    cache_dir      => $php::config::extensioncachedir,
-    configure_params => "--with-yaml=${boxen::config::homebrewdir}", # we need this under OSXs
+    extension         => $extension,
+    version           => $version,
+    package_name      => $package_name,
+    package_url       => $url,
+    homebrew_path     => $boxen::config::homebrewdir,
+    phpenv_root       => $php::config::root,
+    php_version       => $php,
+    cache_dir         => $php::config::extensioncachedir,
+    # we need this under OSXs
+    configure_params  => "--with-yaml=${boxen::config::homebrewdir}",
   }
 
   # Add config file once extension is installed
 
   file { "${php::config::configdir}/${php}/conf.d/${extension}.ini":
-    content => template("php/extensions/generic.ini.erb"),
+    content => template('php/extensions/generic.ini.erb'),
     require => Php_extension[$name],
   }
-
 }
