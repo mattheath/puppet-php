787a79bb5d3a70b04f153f22f09441b2e1103810
Peter Rhoades <createdbypete@gmail.com>
2013-09-01 22:00:43 +0100

    php::fpm::fastcgi spec

---
+++ spec/classes/fpm/php_fpm_fastcgi_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "php::fpm::fastcgi" do
+  let(:facts) { default_test_facts }
+
+  it do
+    should include_class("nginx::config")
+
+    should contain_file("/test/boxen/config/nginx/fastcgi_params").with({
+      :source  => 'puppet:///modules/php/nginx_fastcgi_params',
+      :require => "File[/test/boxen/config/nginx]",
+      :notify  => "Service[dev.nginx]"
+    })
+  end
+end
