022457917d761db6b39dbbff207ef93b6b78fbb5
Peter Rhoades <createdbypete@gmail.com>
2013-09-03 14:16:30 +0100

    APC extension spec with fixture

---
+++ spec/defines/extensions/php_extension_apc_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "php::extension::apc" do
+  let(:facts) { default_test_facts }
+  let(:title) { "apc for 5.4.17" }
+  let(:params) do
+    {
+      :php     => "5.4.17",
+      :version => "3.1.13"
+    }
+  end
+
+  it do
+    should include_class("php::config")
+    should include_class("php::5_4_17")
+
+    should contain_php_extension("apc for 5.4.17").with({
+      :extension      => "apc",
+      :version        => "3.1.13",
+      :package_name   => "APC-3.1.13",
+      :package_url    => "http://pecl.php.net/get/APC-3.1.13.tgz",
+      :homebrew_path  => "/test/boxen/homebrew",
+      :phpenv_root    => "/test/boxen/phpenv",
+      :php_version    => "5.4.17",
+      :cache_dir      => "/test/boxen/data/php/cache/extensions"
+    })
+
+    should contain_file("/test/boxen/config/php/5.4.17/conf.d/apc.ini").with({
+      :content => File.read("spec/fixtures/apc.ini"),
+      :require => "Php_extension[apc for 5.4.17]",
+    })
+  end
+end
