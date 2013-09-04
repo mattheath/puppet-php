9c7f0601e171cf4605bfd01ecd2bda7184a14d07
Peter Rhoades <createdbypete@gmail.com>
2013-09-04 13:49:36 +0100

    Zeromq extension spec required additional fixture module

---
+++ spec/defines/extensions/php_extension_zmq_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "php::extension::zmq" do
+  let(:facts) { default_test_facts }
+  let(:title) { "zmq for 5.4.17" }
+  let(:params) do
+    {
+      :php     => "5.4.17",
+      :version => "1.0.5"
+    }
+  end
+
+  it do
+    should include_class("zeromq")
+    should include_class("php::config")
+    should include_class("php::5_4_17")
+
+    should contain_repository("/test/boxen/data/php/cache/extensions/zmq").with({
+      :source => "mkoppanen/php-zmq"
+    })
+
+    should contain_php_extension("zmq for 5.4.17").with({
+      :provider         => "git",
+      :extension        => "zmq",
+      :version          => "1.0.5",
+      :homebrew_path    => "/test/boxen/homebrew",
+      :phpenv_root      => "/test/boxen/phpenv",
+      :php_version      => "5.4.17",
+      :cache_dir        => "/test/boxen/data/php/cache/extensions",
+      :require          => "Repository[/test/boxen/data/php/cache/extensions/zmq]",
+    })
+
+    should contain_file("/test/boxen/config/php/5.4.17/conf.d/zmq.ini").with({
+      :content => File.read("spec/fixtures/zmq.ini"),
+      :require => "Php_extension[zmq for 5.4.17]"
+    })
+  end
+end
