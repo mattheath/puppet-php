aa72936ff2139d28d74eccba8ea4189e63f22efa
Peter Rhoades <createdbypete@gmail.com>
2013-09-04 10:58:25 +0100

    Intl extension spec

---
+++ spec/defines/extensions/php_extension_intl_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "php::extension::intl" do
+  let(:facts) { default_test_facts }
+  let(:title) { "intl for 5.4.17" }
+  let(:params) do
+    {
+      :php     => "5.4.17",
+      :version => "2.0.1"
+    }
+  end
+
+  it do
+    should include_class("php::config")
+    should include_class("php::5_4_17")
+
+    should contain_php_extension("intl for 5.4.17").with({
+      :extension        => "intl",
+      :version          => "2.0.1",
+      :package_name     => "intl-2.0.1",
+      :package_url      => "http://pecl.php.net/get/intl-2.0.1.tgz",
+      :homebrew_path    => "/test/boxen/homebrew",
+      :phpenv_root      => "/test/boxen/phpenv",
+      :php_version      => "5.4.17",
+      :cache_dir        => "/test/boxen/data/php/cache/extensions",
+      :configure_params => "--with-icu-dir=/test/boxen/homebrew/opt/icu4c"
+    })
+
+    should contain_file("/test/boxen/config/php/5.4.17/conf.d/intl.ini").with({
+      :content => File.read("spec/fixtures/intl.ini"),
+      :require => "Php_extension[intl for 5.4.17]"
+    })
+  end
+end
