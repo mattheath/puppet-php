5dca2709e5749689273064cd4c18a2755e83f249
Peter Rhoades <createdbypete@gmail.com>
2013-09-04 11:04:42 +0100

    Mcrypt extension spec

---
+++ spec/defines/extensions/php_extension_mcrypt_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "php::extension::mcrypt" do
+  let(:facts) { default_test_facts }
+  let(:title) { "mcrypt for 5.4.17" }
+  let(:params) do
+    {
+      :php     => "5.4.17",
+    }
+  end
+
+  it do
+    should include_class("php::config")
+    should include_class("php::5_4_17")
+
+    should contain_php_extension("mcrypt for 5.4.17").with({
+      :provider         => "php_source",
+      :extension        => "mcrypt",
+      :homebrew_path    => "/test/boxen/homebrew",
+      :phpenv_root      => "/test/boxen/phpenv",
+      :php_version      => "5.4.17",
+      :configure_params => "--with-mcrypt=/test/boxen/homebrew/opt/mcrypt",
+    })
+
+    should contain_file("/test/boxen/config/php/5.4.17/conf.d/mcrypt.ini").with({
+      :content => File.read("spec/fixtures/mcrypt.ini"),
+      :require => "Php_extension[mcrypt for 5.4.17]"
+    })
+  end
+end
