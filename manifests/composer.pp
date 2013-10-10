b243f0fa3c0b34f834e0eb955aa04fcb5dd6b1b4
Peter Rhoades <createdbypete@gmail.com>
2013-10-10 22:47:22 +0100

    More direct download would be better

--- manifests/composer.pp
+++ manifests/composer.pp
@@ -8,18 +8,13 @@ class php::composer {
   require php
 
   exec { 'download-php-composer':
-    command => 'curl -sS https://getcomposer.org/installer | /usr/bin/php -d detect_unicode=Off',
-    creates => "${php::config::root}/composer.phar",
+    command => "curl -sS -o ${php::config::root}/bin/composer https://getcomposer.org/composer.phar",
+    creates => "${php::config::root}/bin/composer",
     cwd     => $php::config::root,
     require => Exec[phpenv-setup-root-repo]
   } ->
 
-  file { "${php::config::root}/composer.phar":
-    mode => '0755'
-  } ->
-
   file { "${php::config::root}/bin/composer":
-    ensure => link,
-    target => "${php::config::root}/composer.phar"
+    mode => '0755'
   }
 }
--- spec/classes/composer_spec.rb
+++ spec/classes/composer_spec.rb
@@ -7,19 +7,14 @@ describe "php::composer" do
     should include_class("php")
 
     should contain_exec("download-php-composer").with({
-      :command => "curl -sS https://getcomposer.org/installer | /usr/bin/php -d detect_unicode=Off",
-      :creates => "/test/boxen/phpenv/composer.phar",
+      :command => "curl -sS -o /test/boxen/phpenv/bin/composer https://getcomposer.org/composer.phar",
+      :creates => "/test/boxen/phpenv/bin/composer",
       :cwd     => "/test/boxen/phpenv",
       :require => "Exec[phpenv-setup-root-repo]"
     })
 
-    should contain_file("/test/boxen/phpenv/composer.phar").with({
-      :mode => "0755"
-    })
-
     should contain_file("/test/boxen/phpenv/bin/composer").with({
-      :ensure => "link",
-      :target => "/test/boxen/phpenv/composer.phar"
+      :mode => "0755"
     })
   end
 end
