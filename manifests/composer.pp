2a7ccff33cf509efa47c1081851904d5161ef2df
Peter Rhoades <createdbypete@gmail.com>
2013-10-11 11:27:07 +0100

    Tidy up and correctly name the spec file

--- manifests/composer.pp
+++ manifests/composer.pp
@@ -11,7 +11,7 @@ class php::composer {
     command => "curl -sS -o ${php::config::root}/bin/composer https://getcomposer.org/composer.phar",
     creates => "${php::config::root}/bin/composer",
     cwd     => $php::config::root,
-    require => Exec[phpenv-setup-root-repo]
+    require => Exec['phpenv-setup-root-repo']
   } ->
 
   file { "${php::config::root}/bin/composer":
