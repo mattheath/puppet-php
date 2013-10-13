6e9dc6b04e0f7b2c3b9bc83309d998a902513346
Peter Rhoades <createdbypete@gmail.com>
2013-10-13 17:41:16 +0100

    Updating spec for previous change

--- spec/classes/php_composer_spec.rb
+++ spec/classes/php_composer_spec.rb
@@ -8,7 +8,7 @@ describe "php::composer" do
 
     should contain_exec("download-php-composer").with({
       :command => "curl -sS -o /test/boxen/phpenv/bin/composer http://getcomposer.org/download/1.0.0-alpha7/composer.phar",
-      :creates => "/test/boxen/phpenv/bin/composer",
+      :unless  => "[ -f /test/boxen/phpenv/bin/composer ] && [ \"`md5 -q /test/boxen/phpenv/bin/composer`\" = \"ef51599395560988ea3e16912bfd70f8\" ]",
       :cwd     => "/test/boxen/phpenv",
       :require => "Exec[phpenv-setup-root-repo]"
     })
