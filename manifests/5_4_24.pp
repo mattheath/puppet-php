1553077c409e10411448b64fa8dacd45327e4025
Matt Heath <matt@mattheath.com>
2014-03-15 17:52:09 +0000

    Add 5.4.18 - 5.4.26

---
+++ manifests/5_4_24.pp
@@ -0,0 +1,9 @@
+# Installs php 5.4.24
+#
+# Usage:
+#
+#     include php::5_4_24
+#
+class php::5_4_24 {
+  php::version { '5.4.24': }
+}
---
+++ manifests/5_4_25.pp
@@ -0,0 +1,9 @@
+# Installs php 5.4.25
+#
+# Usage:
+#
+#     include php::5_4_25
+#
+class php::5_4_25 {
+  php::version { '5.4.25': }
+}
