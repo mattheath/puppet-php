907656d8797f2f4d7dd921a1ee1a6642d2b581ee
Nathan Kot <me@nathankot.com>
2014-10-08 12:05:54 +1300

    Add manifests for new php versions.

---
+++ manifests/5_5_15.pp
@@ -0,0 +1,8 @@
+# Installs php 5.5.15
+#
+# Usage:
+#
+#     include php::5_5_15
+class php::5_5_15 {
+  php::version { '5.5.15': }
+}
---
+++ manifests/5_5_16.pp
@@ -0,0 +1,8 @@
+# Installs php 5.5.16
+#
+# Usage:
+#
+#     include php::5_5_16
+class php::5_5_16 {
+  php::version { '5.5.16': }
+}
