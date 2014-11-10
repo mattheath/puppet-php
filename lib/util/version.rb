5fd255737faadaa94f12407203225d54ad26ac95
Sam Bauers <sam@redant.com.au>
2014-11-10 17:24:43 +1100

    Put Version class into it's own file.

---
+++ lib/util/version.rb
@@ -0,0 +1,14 @@
+class Version < Array
+  def initialize s
+    super(s.split('.').map { |e| e.to_i })
+  end
+  def < x
+    (self <=> x) < 0
+  end
+  def > x
+    (self <=> x) > 0
+  end
+  def == x
+    (self <=> x) == 0
+  end
+end
